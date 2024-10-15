da1a1615c336ebdf33aa3af160a27e3ff47e2c94
TowafH <towafh@nycstudents.net>
2024-10-15 19:50:16 -0400

    Add CFU's with Descriptions

--- src/components/CFU.jsx
+++ src/components/CFU.jsx
@@ -2,19 +2,62 @@ function CFU(){
   return(
   <>
         <div className='about-container'>
+
           {/* Container */}
           <div className='about-me'>
-            <h1>CFU #1</h1>
-            <iframe frameborder="0" width="1000px" height="500px" 
+            <h1>CFU #1 - First Program</h1>
+            <iframe frameborder="0" width="1000px" height="600px" 
               src="https://py3.codeskulptor.org/#user309_4VWCix6zN3958GQ.py"></iframe>
-            <p>In this CFU, Blah Blah Blah</p>
+            <p>In <b>CFU #1</b>, the code asks the user for their first and last names, combines them into a full name, and prints a greeting with the full name.</p>
           </div>
 
           <div className='about-me'>
-            <h1>CFU #2</h1>
-            <iframe frameborder="0" width="1000px" height="500px" 
-              src="https://py3.codeskulptor.org/#user309_4VWCix6zN3958GQ.py"></iframe>
-            <p>In this CFU, Blah Blah Blah</p>
+            <h1>CFU #2 - Concatenation Program</h1>
+            <iframe frameborder="0" width="1000px" height="600px" 
+              src="https://py3.codeskulptor.org/#user309_G8bU0SqgT9oT7SL.py"></iframe>
+            <p>In <b>CFU #2</b>, the code retrieves the current year, asks for the user's first name, last name, age, and soccer experience, then calculates the year they started playing soccer. Finally, it prints a message displaying the user's name, age, and soccer history.</p>
+          </div>
+
+          <div className='about-me'>
+            <h1>CFU #3 - Mathematical Operations</h1>
+            <iframe frameborder="0" width="1000px" height="600px" 
+              src="https://py3.codeskulptor.org/#user309_E3Sxb7ymt0sIbJU.py"></iframe>
+            <p>In <b>CFU #3</b>, the code prompts the user to input two numbers and then displays the results of five basic arithmetic operations: addition, subtraction, multiplication, division, and modulus.</p>
+          </div>
+
+          <div className='about-me'>
+            <h1>CFU #4 - Quadratic Formula</h1>
+            <iframe frameborder="0" width="1000px" height="600px" 
+              src="https://py3.codeskulptor.org/#user309_sXZKm4qYdrp6JOU.py"></iframe>
+            <p>In <b>CFU #4</b>, the code introduces a Quadratic Formula Calculator. It prompts the user for their name and the coefficients (a, b, and c) of a quadratic equation, then calculates and displays the two roots of the equation using the quadratic formula.</p>
+          </div>
+
+          <div className='about-me'>
+            <h1>CFU #5 - Canadian Conversions</h1>
+            <iframe frameborder="0" width="1000px" height="600px" 
+              src="https://py3.codeskulptor.org/#user309_k0I4FhTbQrhUD9X.py"></iframe>
+            <p>In <b>CFU #5</b>, the code helps a user calculate the total amount of money they have from various Canadian coins. After receiving the number of each coin type, it converts the values to cents, calculates the total amount in dollars and cents, and then breaks down the amount into whole dollars and the remaining change.</p>
+          </div>
+
+          <div className='about-me'>
+            <h1>CFU #6 - Advanced Mathemical Operations With Random</h1>
+            <iframe frameborder="0" width="1000px" height="600px" 
+              src="https://py3.codeskulptor.org/#user309_1rbZhjx9C4MVm5x.py"></iframe>
+            <p>In <b>CFU #6</b>, the code generates a random number and prompts the user to input their own number. It then performs various advanced and basic arithmetic operations</p>
+          </div>
+
+          <div className='about-me'>
+            <h1>CFU #7 - Functions</h1>
+            <iframe frameborder="0" width="1000px" height="600px" 
+              src="https://py3.codeskulptor.org/#user309_vnCGxGlCfDVX2b6.py"></iframe>
+            <p>In <b>CFU #7</b>, the code prompts the user to input three numbers and then sends those numbers to two separate functions. The first function calculates and prints the sum of the numbers, while the second function calculates and prints the average of the three numbers.</p>
+          </div>
+
+          <div className='about-me'>
+            <h1>CFU #8 - GrubHub App</h1>
+            <iframe frameborder="0" width="1000px" height="600px" 
+              src="https://py3.codeskulptor.org/#user309_BZtw5CHyb7LuPfu.py"></iframe>
+            <p>In <b>CFU #8</b>, the code asks the user if they ordered delivery. If "yes," it requests the total cost and the number of people sharing the bill, then calculates and displays the cost per person. If the response is anything else, it asks who is cooking.</p>
           </div>
         </div>
   </>

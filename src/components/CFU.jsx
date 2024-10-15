1b75b85a7e9cad2ee6caa5a089f96a3885d86333
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-10-15 13:24:47 +0000

    Add Iframes

---
+++ src/components/CFU.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function CFU(){
+  <>
+    <div>
+        <div className='about-container'>
+          {/* Container */}
+          <div className='about-me'>
+            <h1>CFU #1</h1>
+            <iframe frameborder="0" width="50%" height="50%" 
+              src="https://py3.codeskulptor.org/#user309_4VWCix6zN3958GQ.py"></iframe>
+            <p>In this CFU, Blah Blah Blah</p>
+          </div>
+
+          <div className='about-me'>
+            <h1>CFU #2</h1>
+            <iframe frameborder="0" width="50%" height="50%" 
+              src="https://py3.codeskulptor.org/#user309_4VWCix6zN3958GQ.py"></iframe>
+            <p>In this CFU, Blah Blah Blah</p>
+          </div>
+        </div>
+    </div>
+  </>
+}
+
+export default CFU

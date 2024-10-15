fb2b6b9f676d5d94b6da7925296327b8bfcc5e88
TowafH <towafh@nycstudents.net>
2024-10-15 15:16:53 -0400

    Add Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+// Image Imports
+import profilePic from '../assets/pfp.png'
+
+function Home(){
+return(
+<>
+    <div className='about-container'>
+    {/* Container */}
+        <div className='about-me'>
+        <h1>Who Am I?</h1>
+        <img src={profilePic} className='profile-pic' />
+        <p>I'm Towaf from Queens, New York. I'm a rising junior at Thomas Edison CTE HS with a passion for Computer Science, where I'm currently learning Python!</p>
+    </div>
+
+    <div className='about-me'>
+        <h1>Education</h1>
+        <img className='profile-pic' />
+        <p>Web Development</p>
+        <p>Progamming and Prototyping</p>
+        <p>SY 2024-2025</p>
+        <p>Towaf Hossain</p>
+    </div>
+    </div>
+</>
+)
+}
+
+export default Home

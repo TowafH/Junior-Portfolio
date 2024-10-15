dd178879fd32b112cfb30816d3d81713af9fe928
TowafH <towafh@nycstudents.net>
2024-10-15 15:17:00 -0400

    Update CFU component

--- src/components/CFU.jsx
+++ src/components/CFU.jsx
@@ -1,26 +1,24 @@
-import './App.css'
-
 function CFU(){
+  return(
   <>
-    <div>
         <div className='about-container'>
           {/* Container */}
           <div className='about-me'>
             <h1>CFU #1</h1>
-            <iframe frameborder="0" width="50%" height="50%" 
+            <iframe frameborder="0" width="1000px" height="500px" 
               src="https://py3.codeskulptor.org/#user309_4VWCix6zN3958GQ.py"></iframe>
             <p>In this CFU, Blah Blah Blah</p>
           </div>
 
           <div className='about-me'>
             <h1>CFU #2</h1>
-            <iframe frameborder="0" width="50%" height="50%" 
+            <iframe frameborder="0" width="1000px" height="500px" 
               src="https://py3.codeskulptor.org/#user309_4VWCix6zN3958GQ.py"></iframe>
             <p>In this CFU, Blah Blah Blah</p>
           </div>
         </div>
-    </div>
   </>
+  )
 }
 
 export default CFU

17fbe149e74375e7028c6f9193465b13894f3333
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-10-16 12:31:13 +0000

    Add Labs

--- src/components/Lab.jsx
+++ src/components/Lab.jsx
@@ -1,9 +1,28 @@
+
+
 function Lab(){
     return(
-        <>
-        
-        </>
+    <>
+          <div className='about-container'>
+  
+            {/* Container */}
+            <div className='about-me'>
+              <h1>PS Assignment 1: Silly Sentences</h1>
+              <iframe frameborder="0" width="1000px" height="600px" 
+                src="https://py3.codeskulptor.org/#user309_jDWx3SKdd6k5B36.py"></iframe>
+              <p>In <b>PS Assignment 1: Silly Sentences</b>, the program asks the user for a name, adjectives, adverb, foods, noun, place, and verb. It then outputs silly sentences using these inputs</p>
+            </div>
+  
+            <div className='about-me'>
+              <h1>PS Assignment 2: Room Area</h1>
+              <iframe frameborder="0" width="1000px" height="600px" 
+                src="https://py3.codeskulptor.org/#user309_a3avgPGRxQH3c1s.py"></iframe>
+              <p>In <b>PS Assignment 1: Room Area</b>, the code calculates the total area of a room consisting of a bottom rectangle, a middle rectangle, and a triangle based on user-provided side lengths.</p>
+            </div>
+  
+          </div>
+    </>
     )
-}
-
-export default Lab
+  }
+  
+  export default Lab

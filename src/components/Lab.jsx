a67bdcd62bd0460f61bb148448486aae4206a14a
TowafH <towafh@nycstudents.net>
2024-10-30 17:57:13 -0400

    Add Recent PS Assignments

--- src/components/Lab.jsx
+++ src/components/Lab.jsx
@@ -17,7 +17,21 @@ function Lab(){
               <h1>PS Assignment 2: Room Area</h1>
               <iframe frameborder="0" width="1000px" height="600px" 
                 src="https://py3.codeskulptor.org/#user309_a3avgPGRxQH3c1s.py"></iframe>
-              <p>In <b>PS Assignment 1: Room Area</b>, the code calculates the total area of a room consisting of a bottom rectangle, a middle rectangle, and a triangle based on user-provided side lengths.</p>
+              <p>In <b>PS Assignment 2: Room Area</b>, the code calculates the total area of a room consisting of a bottom rectangle, a middle rectangle, and a triangle based on user-provided side lengths.</p>
+            </div>
+
+            <div className='about-me'>
+              <h1>PS Assignment 3: Chatbot</h1>
+              <iframe frameborder="0" width="1000px" height="600px" 
+                src="https://py3.codeskulptor.org/#user309_RBYd0Ha2mGOkMAr.py"></iframe>
+              <p>In <b>PS Assignment 3: Chatbot</b>, I created a basic chatbot that interacts with the user through inputs, responding with personalized messages based on their name, shoe preference, age, clothing choices, and farewell.</p>
+            </div>
+
+            <div className='about-me'>
+              <h1>PS Assignment 4: Divisible By 3</h1>
+              <iframe frameborder="0" width="1000px" height="600px" 
+                src="https://py3.codeskulptor.org/#user309_Z12VVSZ63bFSd7c.py"></iframe>
+              <p>In <b>PS Assignment 4: Divisible By 3</b>, the code checks multiple user-inputted numbers for divisibility by 3 and counts how many are divisible or not, then displays the results.</p>
             </div>
   
           </div>

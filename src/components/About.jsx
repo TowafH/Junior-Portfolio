883ff3090c8a7ae6630725b2b1d0582fa359029e
TowafH <towafh@nycstudents.net>
2024-11-07 20:01:35 -0500

    Add Content for Resume page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,13 @@
+import resume1 from '../assets/resume1.jpg'
+import resume2 from '../assets/resume2.jpg'
+
 function About(){
     return(
         <>
-        
+        <div className='resumeContainer'>
+            <img className="resume" src={resume1}></img>
+            <img className="resume" src={resume2}></img>
+        </div>
         </>
     )
 }
